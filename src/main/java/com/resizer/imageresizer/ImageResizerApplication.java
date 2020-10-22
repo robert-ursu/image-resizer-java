f13c6030c04883e3b308f01ecbf992f1565b0c5a
Robert Ursu <robert.ursu@icloud.com>
2020-10-22 18:29:04 +0300

    Initial commit

---
+++ src/main/java/com/resizer/imageresizer/ImageResizerApplication.java
@@ -0,0 +1,12 @@
+package com.resizer.imageresizer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageResizerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ImageResizerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/resizer/imageresizer/ImageResizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resizer.imageresizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageResizerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
