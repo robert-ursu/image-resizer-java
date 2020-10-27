3943c20b157ef2bf81e5535ab0382ec7b82f7b44
Robert Ursu <robert.ursu@icloud.com>
2020-10-27 21:28:17 +0200

    remove useless configuration

--- src/main/java/com/resizer/imageresizer/property/AppProperties.java
+++ src/main/java/com/resizer/imageresizer/property/AppProperties.java
@@ -1,7 +1,9 @@
 package com.resizer.imageresizer.property;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
   private String storagePath;
